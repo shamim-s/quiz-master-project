dd0e3485e2b360b05c6b188c2d2841173a75ab1c
Shamim Sarkar <shamimibas@gmail.com>
2022-10-11 12:25:47 +0600

    Navbar added

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='h-16 bg-cyan-500 flex justify-between items-center pl-12 pr-12'>
+            <Link to={'/'}><h1 className='text-2xl font-bold text-white'>QUIZ-MASTER</h1></Link>
+            <div className='text-white text-lg font-bold'>
+                <NavLink to={'/home'} className='ml-12'>HOME</NavLink>
+                <NavLink to={'/statistic'} className='ml-12'>STATISTIC</NavLink>
+                <NavLink to={'/more'} className='ml-12'>MORE</NavLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/QuizCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizCard = () => {
+    return (
+        <div>
+            <h1>Quiz Card</h1>
+        </div>
+    );
+};
+
+export default QuizCard;
